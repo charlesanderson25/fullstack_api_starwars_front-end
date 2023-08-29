f6f3d01064a3df72c871b97323c8c1611474c562
charlesanderson25 <charles_spfc@hotmail.com>
2023-08-29 11:12:26 -0300

    Clear code

--- SWAPI/src/components/Button.tsx
+++ SWAPI/src/components/Button.tsx
@@ -1,22 +1,3 @@
-// import React, { ButtonHTMLAttributes } from "react";
-
-// interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-//   children: React.ReactNode;
-// }
-
-// const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
-//   return (
-//     <button
-//       className="bg-f9004d hover:bg-ffcdb8 text-white p-3 rounded-md cursor-pointer"
-//       {...rest}
-//     >
-//       {children}
-//     </button>
-//   );
-// };
-
-// export default Button;
-
 import React, { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
