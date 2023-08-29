620f7023f71f024a1fef6c5e9db0a3c5b6a83619
charlesanderson25 <charles_spfc@hotmail.com>
2023-08-29 11:12:03 -0300

    Clear code

--- SWAPI/src/components/Banner.tsx
+++ SWAPI/src/components/Banner.tsx
@@ -1,17 +1,3 @@
-// import React from "react";
-
-// const Banner: React.FC = () => {
-//   return (
-//     <div className="bg-darkTheme p-6 sm:p-8 md:p-12 lg:p-16 text-white">
-//       <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-//         Pesquise seu personagem - Star Wars
-//       </h1>
-//     </div>
-//   );
-// };
-
-// export default Banner;
-
 import React from "react";
 
 const textBanner = {
