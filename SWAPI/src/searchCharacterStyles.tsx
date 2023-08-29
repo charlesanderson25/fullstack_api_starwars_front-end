43bfe2e8a7c9f175065b988e6933b71095b1b2df
charlesanderson25 <charles_spfc@hotmail.com>
2023-08-29 11:08:24 -0300

    Add textSearchCharacterStyles file

---
+++ SWAPI/src/searchCharacterStyles.tsx
@@ -0,0 +1,49 @@
+const textSearchCharacterStyles = {
+  container: {
+    backgroundColor: "#1a202c",
+    padding: "1.5rem 2rem",
+    height: "80vh",
+    width: "100%",
+  },
+  form: {
+    display: "flex",
+  },
+  input: {
+    width: "100%",
+    backgroundColor: "#2d3748",
+    color: "white",
+    padding: "0.75rem",
+    borderRadius: "0.25rem 0 0 0.25rem",
+    border: "none",
+  },
+  button: {
+    backgroundColor: "#f9004d",
+    hoverBackgroundColor: "#ffcdb8",
+    color: "white",
+    padding: "0.75rem 1rem",
+    borderRadius: "0 0.25rem 0.25rem 0",
+    cursor: "pointer",
+    border: "none",
+  },
+  result: {
+    marginTop: "1rem",
+  },
+  characterInfo: {
+    color: "white",
+  },
+  favorites: {
+    marginTop: "2rem",
+    color: "#f9004d",
+    fontFamily: "Josefin Sans, sans-serif",
+  },
+  favoritesList: {
+    listStyleType: "none",
+    paddingLeft: 0,
+  },
+  favoriteItem: {
+    marginBottom: "0.5rem",
+    color: "white",
+  },
+};
+
+export default textSearchCharacterStyles;
